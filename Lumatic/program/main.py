80207e9b2fd86f87aff847444335541a3e860b24
Telory Frozy <135508142+teloryfrozy@users.noreply.github.com>
2023-06-04 13:45:43 +0200

    Add files via upload

---
+++ Lumatic/program/main.py
@@ -0,0 +1,9 @@
+############################
+#   Lumatic Version 1.0    #
+#   Author: La Bro'tique   #
+############################
+from platform import system
+from gui import GUI
+
+# Launch the application
+GUI(system())
