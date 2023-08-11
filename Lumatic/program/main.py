e2ad847db89adbfad9da9622e608ecc7f2def83c
Telory Frozy <135508142+teloryfrozy@users.noreply.github.com>
2023-08-11 19:16:34 +0200

    Delete Lumatic directory

--- Lumatic/program/main.py
+++
@@ -1,9 +0,0 @@
-############################
-#   Lumatic Version 1.0    #
-#   Author: La Bro'tique   #
-############################
-from platform import system
-from gui import GUI
-
-# Launch the application
-GUI(system())
